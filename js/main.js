6d3c03c38d90389f2f5cac8b482557ac61228bb6
Kevin <ktripp1@gmail.com>
2014-08-15 15:57:51 -0400

    finished mobile nav support

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-	
+	//Expands and retracts the search bar when it becomes selected.
+	$('nav input').on('focus',function(){
+		$(this).attr('data-default', $(this).width());
+		$(this).animate({ width: 115 }, 'fast');
+	}).on('blur', function(){
+		var w = $(this).attr('data-default');
+		$(this).animate({width: w }, 'fast');
+	});
+
+	$('nav .hamburger, nav .close-btn').on('click',function(){
+		console.log($(this))
+		if (!( $('nav').css('width') == $('body').css('width'))){
+			$('nav').toggleClass('open');
+				$('nav .close-btn').toggleClass('show');
+				$('nav .hamburger').toggleClass('show');
+		};
+	});
 });
