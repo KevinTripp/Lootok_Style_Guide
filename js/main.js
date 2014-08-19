ea5bc46e8c648b91fe3e4fc1f3ce10feec74b482
Kevin <ktripp1@gmail.com>
2014-08-19 17:28:56 -0400

    fixed nav, added forms and footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	//Expands and retracts the search bar when it becomes selected.
 	$('nav input').on('focus',function(){
+		
 		$(this).attr('data-default', $(this).width());
 		$(this).animate({ width: 115 }, 'fast');
 	}).on('blur', function(){
@@ -9,11 +10,20 @@ $(document).ready(function(){
 	});
 
 	$('nav .hamburger, nav .close-btn').on('click',function(){
-		console.log($(this))
 		if (!( $('nav').css('width') == $('body').css('width'))){
-			$('nav').toggleClass('open');
-				$('nav .close-btn').toggleClass('show');
-				$('nav .hamburger').toggleClass('show');
+			$('nav ul').first().toggleClass('hide');//hides the ul when menu is collapsed
+			$('nav').toggleClass('open');//changes the width of the menu
+			$('nav .close-btn').toggleClass('show');
+			$('nav .hamburger').toggleClass('show');
 		};
+	});
+	//shows the confirmation screen on submit
+	$('form button').on('click',function(e){
+		e.preventDefault();
+		$('.submit-confirm, .overlay').toggle();
+	});
+	//hides the confirmation screen when x is clicked
+	$('.submit-confirm .close-btn, .overlay').on('click',function(){
+		$('.submit-confirm, .overlay').toggle();
 	});
 });
